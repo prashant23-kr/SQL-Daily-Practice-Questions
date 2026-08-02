f44dcb36b46a070b9fe67bbf96a7f7d0ebf0c634
prashant23-kr <prashant.kumar3948@gmail.com>
2026-08-02 19:20:15 +0530

    Update SQL script for employee database operations

--- 30 QUESTION SQL.sql
+++ 30 QUESTION SQL.sql
@@ -1,6 +1,6 @@
--- CREATING A DATABASE
+-- 1 CREATING A DATABASE
 Create database Company_DB;
--- CREATING A TABLE 
+-- 2 CREATING A TABLE 
 create table employees(
 	emp_id 	INT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
@@ -9,7 +9,7 @@ create table employees(
     salary DECIMAL(10,2) NOT NULL,
     department VARCHAR(50)
     );
--- INSERTING DATA
+-- 3  INSERTING DATA
 
 INSERT INTO employees(emp_id, first_name, last_name, gender, salary, department) Values
 	(101,'Prashant','Kumar','M','55000','IT'),
@@ -18,76 +18,125 @@ INSERT INTO employees(emp_id, first_name, last_name, gender, salary, department)
     (104,'Aman','Verma','M','51000','Sales'),
     (105,'Neha','Gupta','F','59000','IT');
     
--- Display the record
+-- 4 Display the record
 select * from employees;    
 
--- Display all details of employees whose department is IT.
+-- 5 Display all details of employees whose department is IT.
 select * from employees where department = 'IT';
--- Display all employees whose salary is greater than 50,000.
+-- 6 Display all employees whose salary is greater than 50,000.
 select * from employees where department <> 'IT';
 
--- Display all employees who:
+-- 7  Display all employees who:
 -- belong to the IT department AND
 -- have a salary greater than 55,000.
 
 select * from employees where department = 'IT' AND salary > 55000;
 
--- Display all employees who:
+-- 8  Display all employees who:
 -- belong to the IT department OR
 -- belong to the HR department.
 
 select * from employees where department IN ('IT','HR');
 
--- Display all employees who:
+-- 9 Display all employees who:
 -- are in the IT department OR
 -- are in the HR department
 
 select * from employees where department IN ('IT','HR') AND salary > 50000;
--- Display all employees whose salary is between 50,000 and 60,000 (inclusive).
+-- 10 Display all employees whose salary is between 50,000 and 60,000 (inclusive).
 select * from employees where salary NOT between 50000 and 60000;
 
--- Display all employees whose department is IT, HR, or Finance.
+-- 11 Display all employees whose department is IT, HR, or Finance.
 select * from employees where department NOT IN ('IT','HR');
 
--- Display all employees whose first name starts with the letter P.
+-- 12 Display all employees whose first name starts with the letter P.
 select * from employees WHERE first_name LIKE '_r%';
 
--- Display all employees, sorted by salary in ascending order (lowest salary first).
+-- 13 Display all employees, sorted by salary in ascending order (lowest salary first).
 select * from employees order by salary ASC;
 
--- Display all employees, sorted by salary in descending order (highest salary first).
+-- 14 Display all employees, sorted by salary in descending order (highest salary first).
 select * from employees order by salary desc;
 
--- Display the top 3 highest-paid employees.
+-- 15 Display the top 3 highest-paid employees.
 select * from employees order by salary DESC LIMIT 3 ;
--- Display the 2nd and 3rd highest-paid employees.
+-- 16 Display the 2nd and 3rd highest-paid employees.
 
 select * from employees order by salary DESC limit 2 offset 1;
 
--- Find the total number of employees in the employees table.
+-- 17 Find the total number of employees in the employees table.
 select count(*) from employees;
 
--- 👉 Count the number of employees whose department is NOT NULL.
+-- 18 Count the number of employees whose department is NOT NULL.
 select count(*) from employees where department IS NOT null;
 
--- Find the total salary of all employees.
+-- 19 Find the total salary of all employees.
 select min(salary) from employees;
 
--- 👉 Display the number of employees in each department.
+-- 20 Display the number of employees in each department.
 select count(department) from employees group by department;
 
--- Display the average salary of each department.
+-- 21 Display the average salary of each department.
 select department, avg(salary) AS avg_salary from employees group by department;
 
--- 👉 Display the highest salary in each department.
+-- 22 Display the highest salary in each department.
 select department,max(salary) as max_salary from employees group by department;
 
--- Display only those departments where the maximum salary is greater than 55,000.
+-- 23 Display only those departments where the maximum salary is greater than 55,000.
 
 SELECT department, MAX(salary) AS max_sal
 FROM employees
 GROUP BY department
 HAVING MAX(salary) > 55000;
 
+-- 24 ABOVE TOP 30 QUESTIONN
+-- both question pratice one time more
 -- Display only those departments that have more than 1 employee.
-select 
+select department, count(*) from employees group by department having count(*) > 1; 
+
+-- 25 Display the number of employees in each department, sorted by the employee count in descending order
+select department, count(*) AS total_eemployee from employees group by department order by count(*) DESC;
+
+-- 26 Count the number of employees for each department and gender.
+select department,gender,count(*) AS no_emp from employees group by department, gender;
+
+-- 27 Show the departments where male ('M') employees have an average salary greater than 50,000.
+select department,avg(salary) AS emp_sal from employees where gender = 'M' group by department having avg(salary) > 50000;
+
+
+-- 28 DISTINCT QUESTION
+-- Display all unique department names from the employees table.
+select distinct department from employees;
+
+-- 29 Find the total number of unique departments in the employees table.
+select count(distinct department) as UNIQUE_DEP from employees;
+
+-- IS NULL
+-- 30 Display all employees whose department is NULL.
+select * from employees where department is null;
+
+-- 31 Update Rahul's salary to ₹52,000.
+UPDATE employees
+SET salary = 52000
+WHERE first_name = 'Priya';
+
+-- 32 Delete the employee Aman from the employees table.
+delete from employees where first_name = 'Aman';
+
+-- 33 Add a new column named email to the employees table.
+ALTER TABLE employees ADD COLUMN email varchar(50);
+
+-- 34 Remove the email column from the employees table. 
+ALTER TABLE employees DROP COLUMN email;
+
+-- 35 Rename the column first_name to fname.
+ALTER table employees RENAME column first_name TO fname;
+
+-- 36 Change the salary column from:
+ALTER TABLE employees modify column salary  DECIMAL(12,2); 
+
+-- 37 Rename the column department to dept and keep its data type as VARCHAR(50).
+ALTER TABLE employees CHANGE COLUMN department dept VARCHAR(50);
+
+
+-- JOIN QUESTION 
