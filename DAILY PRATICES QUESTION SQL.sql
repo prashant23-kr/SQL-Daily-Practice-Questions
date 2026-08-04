a031ad75ec4095d3bf8362da5113c1d0e2a0c1b7
prashant23-kr <prashant.kumar3948@gmail.com>
2026-08-04 13:09:06 +0530

    Update more question like joins
    Created the department table, added dept_id to employees, and updated employee department assignments. Added various SQL queries for joining employees with departments and calculating aggregates.

--- DAILY PRATICES QUESTION SQL.sql
+++ DAILY PRATICES QUESTION SQL.sql
@@ -140,3 +140,188 @@ ALTER TABLE employees CHANGE COLUMN department dept VARCHAR(50);
 
 
 -- JOIN QUESTION 
+
+-- 38 Create the departments table
+Create table department(
+	dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
+    );
+    
+-- 39 insert Data into departments
+INSERT INTO department (dept_id,dept_name)
+	values(1,'IT'),
+		  (2,'HR'),
+          (3,'Finance'),
+          (4,'Sales');
+
+-- 40 Add a new column named dept_id to the employees table.
+ALTER TABLE employees ADD COLUMN dept_id INT;
+
+-- 41 Update Prashant's dept_id to 1.
+UPDATE employees SET dept_id = 1 WHERE fname = 'Prashant';
+ -- 42 Now update the other employees' dept_id values.
+ 
+ UPDATE employees SET dept_id = CASE
+	WHEN fname = 'Rahul' then 2
+	when fname = 'Priya' then 3
+    WHEN fname = 'Aman' then 4
+    WHEN fname = 'Neha' then 1
+END
+where fname IN ('Rahul','Priya','Aman','Neha');
+
+-- Inner join
+-- 43 using an INNER JOIN between: employees, departments
+Select e.fname,
+	   d.dept_name
+	From employees e
+    INNER JOIN department d
+    on e.dept_id = d.dept_id;
+    
+-- left join
+-- 44 Display all employees along with their department names, even if an employee does not have a matching department.
+Select e.fname,
+	   d.dept_name 	
+	from employees e LEFT JOIN department d
+on e.fname = d.dept_id;
+
+-- Right Join
+-- 45 Display all departments, including those that have no employees.
+select e.fname, d.dept_name
+	from employees e RIGHT JOIN department d
+on d.dept_id = d.dept_id;
+
+-- 46 Display the employee name and department name. (INNER JOIN)
+
+select e.fname, d.dept_name
+	from employees e
+    INNER JOIN department d
+on e.dept_id = d.dept_id;
+Select * from employees;
+-- 47 Display all employees, even if they don't have a department. (LEFT JOIN)
+select e.fname, d.dept_name
+from employees e 
+LEFT JOIN department d
+ON e.dept_id = d.dept_id;
+
+-- 48 Display: Employee Name Department Name Salary using an INNER JOIN.
+select e.fname, d.dept_name, e.salary
+from employees e INNER JOIN department d
+on e.dept_id = e.dept_id;
+
+-- 04/08/2026
+
+-- 49 Display: Employee Name (fname) Department Name (dept_name) Only for employees who work in the IT department.
+select e.fname, d.dept_name
+from employees e 
+Inner join department d
+on e.dept_id = d.dept_id
+where d.dept_name = 'IT';
+
+-- 50 Display: fname salary, dept_name,Only employees whose salary is greater than 50000.
+select e.fname, d.dept_name, salary
+from employees e 
+Inner join department d
+on e.dept_id = d.dept_id
+where salary > 50000;
+
+-- 51 Display: fname dept_name salary Only employees who work in the IT department and have a salary greater than 55,000.
+select e.fname, d.dept_name, salary
+from employees e 
+inner join department d
+on e.dept_id = d.dept_id
+where dept_name = 'IT' AND salary > 55000;
+
+-- 52 Display: fname, dept_name, salary ,Only employees who work in either IT or HR departments and whose salary is greater than 50,000
+select e.fname,
+       d.dept_name,	
+       e.salary
+from employees e 
+Inner join department d
+on e.dept_id = d.dept_id 
+where dept_name in ('IT','HR') and e.salary > 50000;
+
+-- 53 Display: fname dept_name salary Show only employees who are in the IT or Finance department and whose salary is between 50000 and 60000.
+
+select e.fname,
+	   d.dept_name,
+       e.salary
+from employees e 
+Inner join department d 
+on e.dept_id = d.dept_id
+where d.dept_name IN ('IT','Finance') AND Salary between 50000 and 60000;
+
+-- 54 Display: fname, dept_name, salary
+-- Show employees who:
+-- belong to the IT, HR, or Finance department,
+-- have a salary greater than or equal to 50,000,
+-- and sort the results by salary in descending order.
+select e.fname, d.dept_name, e.salary
+from employees e
+inner join department d
+on e.dept_id = d.dept_id
+where d.dept_name IN ('IT','HR','Finance') 
+AND e.salary >= 50000 order by e.salary desc;
+
+-- 55 Display: dept_name 
+-- Total number of employees in each department.
+
+select d.dept_name 
+from employees e
+inner join department d
+on e.dept_id = d.dept_id
+where count(*) group by d.dept_name order by employees;
+
+-- 56 Display: d.dept_name AVG(e.salary) as avg_salary
+-- Show only those departments whose average salary is greater than 55,000.
+select d.dept_name, AVG(e.salary) as avg_salary 
+from employees e
+inner join department d
+on e.dept_id = d.dept_id
+group by dept_name having avg(e.salary) > 550000;
+
+-- 57 Display Department Name Total Salary of each department
+select dept_name, sum(e.salary) as total_sal
+from employees e
+inner join department d
+on e.dept_id = d.dept_id
+group by d.dept_name;
+
+-- 58 Display: dept_name SUM(e.salary) as total_salary
+-- Show only those departments whose total salary is greater than 100000.
+select dept_name, SUM(e.salary) as total_salary
+from employees e
+inner join department d
+on e.dept_id  = d.dept_id
+group by dept_name having  sum(e.salary) >100000;
+
+-- 59 Display: dept_name COUNT(*) as total_emp
+-- Show only those departments that have more than 1 employee.
+
+select dept_name, count(*) as total_emp
+from employees e
+inner join department d
+on e.dept_id = d.dept_id
+group by dept_name having count(*) > 1;
+
+-- 60 Display: dept_name COUNT(*) AS total_emp
+-- Conditions:
+-- Consider only employees whose salary is greater than 50,000.
+-- Then display only those departments having more than 1 employee.
+
+select dept_name, count(*) as total_emp
+from employees e
+inner join department d
+on e.dept_id = d.dept_id
+ where salary > 50000 group by dept_name having count(*) > 1;
+ 
+-- 61 Display: dept_name AVG(e.salary) as avg_salary
+-- Conditions:
+-- Consider only employees whose salary is greater than or equal to 50,000.
+-- Show only departments whose average salary is greater than 55,000.
+-- Sort by avg_salary in descending order.
+select dept_name, Avg(e.salary) as avg_salary
+from employees e
+INNER JOIN department d
+on e.dept_id = d.dept_id
+where salary >= 50000 group by dept_name having avg(e.salary) > 55000
+order by avg_salary desc;
